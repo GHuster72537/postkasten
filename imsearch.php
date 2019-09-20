56465ec32fc24bb4897c6c2efa5d2be20ef74f5f
GHuster72537 <55576336+GHuster72537@users.noreply.github.com>
2019-09-21 00:04:11 +0200

    Add files via upload

--- imsearch.php
+++ imsearch.php
@@ -1,14 +1,15 @@
+<?php require_once("res/x5engine.php"); ?>
 <!DOCTYPE html><!-- HTML5 -->
 <html prefix="og: http://ogp.me/ns#" lang="de-DE" dir="ltr">
 	<head>
-		<title>Postkasten.cf</title>
+		<title>Suchen - Postkasten.cf</title>
 		<meta charset="utf-8" />
 		<!--[if IE]><meta http-equiv="ImageToolbar" content="False" /><![endif]-->
 		<meta name="generator" content="Incomedia WebSite X5 Professional 16.2.1 - www.websitex5.com" />
 		<meta property="og:locale" content="de" />
 		<meta property="og:type" content="website" />
-		<meta property="og:url" content="https://postkasten.cf/index.html" />
-		<meta property="og:title" content="Homepage" />
+		<meta property="og:url" content="https://postkasten.cf/imsearch.php" />
+		<meta property="og:title" content="Suchen" />
 		<meta property="og:site_name" content="Postkasten.cf" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		
@@ -18,14 +19,14 @@
 		<link rel="stylesheet" href="style/template.css?16-2-1-0" media="screen" />
 		
 		<link rel="stylesheet" href="pluginAppObj/imFooter_pluginAppObj_09/custom.css" media="screen, print" />
-		<link rel="stylesheet" href="pcss/index.css?16-2-1-0-637046204064399604" media="screen,print" />
+		<link rel="stylesheet" href="pcss/imsearch.css?16-2-1-0-637046204064429653" media="screen,print" />
 		<script src="res/jquery.js?16-2-1-0"></script>
 		<script src="res/x5engine.js?16-2-1-0" data-files-version="16-2-1-0"></script>
 		
 		<script src="pluginAppObj/imFooter_pluginAppObj_09/main.js"></script>
 		<script>
 			window.onload = function(){ checkBrowserCompatibility('Der von Ihnen verwendete Browser unterstützt nicht die die Funktionen, die für die Anzeige dieser Website benötigt werden.','Der von Ihnen verwendete Browser unterstützt möglicherweise nicht die die Funktionen, die für die Anzeige dieser Website benötigt werden.','[1]Browser aktualisieren[/1] oder [2]Vorgang fortsetzen[/2].','http://outdatedbrowser.com/'); };
-			x5engine.utils.currentPagePath = 'index.html';
+			x5engine.utils.currentPagePath = 'imsearch.php';
 			x5engine.boot.push(function () { x5engine.imPageToTop.initializeButton({}); });
 		</script>
 		
@@ -37,7 +38,7 @@
 				<div id="imFooterBg"></div>
 				<div id="imPage">
 					<header id="imHeader">
-						<h1 class="imHidden">Postkasten.cf</h1>
+						<h1 class="imHidden">Suchen - Postkasten.cf</h1>
 						<div id="imHeaderObjects"><div id="imHeader_imObjectImage_08_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_8" class="" > <div id="imHeader_imCellStyleGraphics_8"></div><div id="imHeader_imCellStyle_8" ><img id="imHeader_imObjectImage_08" src="images/raw-3-.png" title="" alt="" /></div></div></div><div id="imHeader_imTextObject_05_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_5" class="" > <div id="imHeader_imCellStyleGraphics_5"></div><div id="imHeader_imCellStyle_5" ><div id="imHeader_imTextObject_05">
 	<div class="text-tab-content"  id="imHeader_imTextObject_05_tab0" style="">
 		<div class="text-inner">
@@ -159,6 +160,17 @@ $(function () {$('#imStickyBar_imMenuObject_01 ul li').not('.imMnMnSeparator').e
 						<div id="imContentGraphics"></div>
 						<main id="imContent">
 							<a id="imGoToCont"></a>
+				<div id="imSearchPage">
+				<h2 id="imPgTitle">Suchergebnisse</h2>
+				<?php
+				$search = new imSearch();
+				$keys = isset($_GET['search']) ? @htmlspecialchars($_GET['search']) : "";
+				$page = isset($_GET['page']) ? @htmlspecialchars($_GET['page']) : 0;
+				$type = isset($_GET['type']) ? @htmlspecialchars($_GET['type']) : "pages"; ?>
+				<div class="searchPageContainer">
+				<?php echo $search->search($keys, $page, $type); ?>
+				</div>
+				</div>
 				
 							<div class="imClear"></div>
 						</main>
---
+++ res/access.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+// Users data
+$imSettings['access']['webregistrations_gid'] = 'ngg50f8o';
+$imSettings['access']['users'] = array(
+	'hosting_evolutionx5@postkasten.cf' => array(
+		'groups' => array('qszg0cd1'),
+		'id' => 'dse11q31',
+		'firstname' => 'Admin',
+		'lastname' => '',
+		'password' => 'gVF0cP45oY6f',
+		'email' => 'hosting_evolutionx5@postkasten.cf',
+		'page' => false
+	)
+);
+
+// Admins list
+$imSettings['access']['admins'] = array('dse11q31');
+
+// Page/Users permissions
+$imSettings['access']['pages'] = array();
+
+// End of file access.inc.php
